379665ac471731df5418e56fb271b5846ff49f29
RafaelNevesdeOliveira <rafaelneves652@gmail.com>
2024-02-23 11:42:44 -0300

    rotas finalzadas

--- README.md
+++ README.md
@@ -8,14 +8,14 @@
 - Data e Hora
 - Está dentro ou não da dieta
 
-- [ ] Deve ser possível editar uma refeição, podendo alterar todos os dados acima
-- [ ] Deve ser possível apagar uma refeição
-- [ ] Deve ser possível listar todas as refeições de um usuário
-- [ ] Deve ser possível visualizar uma única refeição
-- [ ] Deve ser possível recuperar as métricas de um usuário
+- [x] Deve ser possível editar uma refeição, podendo alterar todos os dados acima
+- [x] Deve ser possível apagar uma refeição
+- [x] Deve ser possível listar todas as refeições de um usuário
+- [x] Deve ser possível visualizar uma única refeição
+- [x] Deve ser possível recuperar as métricas de um usuário
 - Quantidade total de refeições registradas
 - Quantidade total de refeições dentro da dieta
 - Quantidade total de refeições fora da dieta
 - Melhor sequência de refeições dentro da dieta
 
-- [ ] O usuário só pode visualizar, editar e apagar as refeições o qual ele criou
+- [x] O usuário só pode visualizar, editar e apagar as refeições o qual ele criou
--- src/routes/meals.ts
+++ src/routes/meals.ts
@@ -16,19 +16,13 @@ export async function mealsRoutes(app: FastifyInstance) {
         date: z.coerce.date(),
       })
 
-      let sessionId = request.cookies.sessionId
-
-      if (!sessionId) {
-        sessionId = randomUUID()
-
-        reply.cookie('sessionId', sessionId, {
-          path: '/',
-          maxAge: 3600 * 24 * 7, // 7 dias
-        })
-      }
       try {
         const { name, description, isOnDiet, date } =
           createMealBodySchema.parse(request.body)
+
+        if (!request.user) {
+          return reply.status(400).send('Nenhum usuário cadastrado')
+        }
 
         await knex('meals').insert({
           id: randomUUID(),
@@ -40,12 +34,174 @@ export async function mealsRoutes(app: FastifyInstance) {
         })
         return reply.status(201).send('Refeição Criada')
       } catch (error) {
-        if (!request.user?.id) {
-          return reply.status(400).send('Nenhum usuário cadastrado')
-        }
-
         return reply.status(500).send('Erro ao criar refeição')
       }
+    },
+  )
+
+  app.get(
+    '/',
+    { preHandler: [checkSessionIdExists] },
+    async (request, reply) => {
+      const meals = await knex('meals')
+        .where({
+          user_id: request.user?.id,
+        })
+        .orderBy('date', 'desc')
+      const mealsData = meals.map((meal) => ({
+        ...meal,
+        date: new Date(meal.date).toISOString(),
+      }))
+
+      return reply.send({
+        mealsData,
+      })
+    },
+  )
+
+  app.get(
+    '/:mealId',
+    { preHandler: [checkSessionIdExists] },
+    async (request, reply) => {
+      const getMealParamsSchema = z.object({
+        mealId: z.string().uuid(),
+      })
+
+      try {
+        const { mealId } = getMealParamsSchema.parse(request.params)
+
+        const meal = await knex('meals')
+          .where({
+            id: mealId,
+          })
+          .first()
+
+        if (!meal) {
+          return reply.status(404).send('Refeição não encontrada')
+        }
+
+        return reply.send({
+          meal,
+        })
+      } catch (error) {
+        console.error(error)
+        return reply.status(500).send('Erro ao processar a solicitação')
+      }
+    },
+  )
+
+  app.delete(
+    '/:mealId',
+    { preHandler: [checkSessionIdExists] },
+    async (request, reply) => {
+      const getMealParamsSchema = z.object({
+        mealId: z.string().uuid(),
+      })
+
+      try {
+        const { mealId } = getMealParamsSchema.parse(request.params)
+
+        const meal = await knex('meals')
+          .where({
+            id: mealId,
+          })
+          .first()
+
+        if (!meal) {
+          return reply.status(404).send('Refeição não encontrada')
+        }
+        await knex('meals')
+          .where({
+            id: mealId,
+          })
+          .delete()
+
+        return reply.status(204).send()
+      } catch (error) {
+        return reply.status(500).send('Erro ao processar a solicitação')
+      }
+    },
+  )
+
+  app.put(
+    '/:mealId',
+    { preHandler: [checkSessionIdExists] },
+    async (request, reply) => {
+      const paramsSchema = z.object({ mealId: z.string().uuid() })
+
+      const { mealId } = paramsSchema.parse(request.params)
+
+      const createMealBodySchema = z.object({
+        name: z.string(),
+        description: z.string(),
+        isOnDiet: z.boolean(),
+        date: z.coerce.date(),
+      })
+
+      try {
+        const { name, description, isOnDiet, date } =
+          createMealBodySchema.parse(request.body)
+
+        const meal = await knex('meals').where({ id: mealId }).first()
+
+        if (!meal) {
+          return reply.status(404).send({ error: 'Meal not found' })
+        }
+
+        await knex('meals').update({
+          name,
+          description,
+          is_on_diet: isOnDiet,
+          date: date.getTime(),
+        })
+        return reply.status(204).send('Refeição editada')
+      } catch (error) {
+        return reply.status(500).send('Erro ao criar refeição')
+      }
+    },
+  )
+
+  app.get(
+    '/metrics',
+    { preHandler: [checkSessionIdExists] },
+    async (request, reply) => {
+      const totalMealsOnDiet = await knex('meals')
+        .where({ user_id: request.user?.id, is_on_diet: true })
+        .count('id', { as: 'total' })
+        .first()
+
+      const totalMealsOffDiet = await knex('meals')
+        .where({ user_id: request.user?.id, is_on_diet: false })
+        .count('id', { as: 'total' })
+        .first()
+
+      const totalMeals = await knex('meals')
+        .where({ user_id: request.user?.id })
+        .orderBy('date', 'desc')
+
+      const { bestOnDietSequence } = totalMeals.reduce(
+        (acc, meal) => {
+          if (meal.is_on_diet) {
+            acc.currentSequence += 1
+          } else {
+            acc.currentSequence = 0
+          }
+
+          if (acc.currentSequence > acc.bestOnDietSequence) {
+            acc.bestOnDietSequence = acc.currentSequence
+          }
+
+          return acc
+        },
+        { bestOnDietSequence: 0, currentSequence: 0 },
+      )
+
+      return reply.send({
+        totalMeals: totalMeals.length,
+        totalMealsOnDiet: totalMealsOnDiet?.total,
+        totalMealsOffDiet: totalMealsOffDiet?.total,
+        bestOnDietSequence,
+      })
     },
   )
 }
