c860def0694a351099fe8d3f51a566847a233028
RafaelNevesdeOliveira <rafaelneves652@gmail.com>
2024-02-27 15:53:45 -0300

    tests

--- src/env/index.ts
+++ src/env/index.ts
@@ -1,8 +1,15 @@
-import 'dotenv/config'
+import { config } from 'dotenv'
 import { z } from 'zod'
+
+if (process.env.NODE_ENV === 'test') {
+  config({ path: '.env.test' })
+} else {
+  config()
+}
 
 const envSchema = z.object({
   NODE_ENV: z.enum(['development', 'test', 'production']).default('production'),
+  DATABASE_CLIENT: z.enum(['sqlite', 'pg']).default('sqlite'),
   DATABASE_URL: z.string(),
   PORT: z.number().default(3333),
 })
---
+++ test/users.spec.ts
@@ -0,0 +1,32 @@
+import { expect, it, describe, beforeAll, afterAll, beforeEach } from 'vitest'
+import request from 'supertest'
+import { app } from '../src/app'
+import { execSync } from 'node:child_process'
+
+describe('Users routes', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
+  beforeEach(() => {
+    execSync('npm run knex migrate:rollback --all')
+    execSync('npm run knex migrate:latest')
+  })
+
+  it('should be able to create a new user', async () => {
+    const response = await request(app.server)
+      .post('/users')
+      .send({ name: 'John Doe', email: 'johndoe@gmail.com' })
+      .expect(201)
+
+    const cookies = response.get('Set-Cookie')
+
+    expect(cookies).toEqual(
+      expect.arrayContaining([expect.stringContaining('sessionId')]),
+    )
+  })
+})
